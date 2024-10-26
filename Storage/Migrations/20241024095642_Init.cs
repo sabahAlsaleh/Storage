792c9a10a3a03a99c8fbc82bc1ac5e19767e611c
Sabah <sabahaa@kth.se>
2024-10-27 01:24:40 +0200

    Add project files.

---
+++ Storage/Migrations/20241024095642_Init.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Orderdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Shelf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Storage/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Range(1, 30000)]
+        public int Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Order Date")]
+        public DateTime Orderdate { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [StringLength(10)]
+        public string Shelf { get; set; }
+
+        [Range(0, 1000)]
+        public int Count { get; set; }
+        
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
